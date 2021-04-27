9ccf97b5b9fdb6a7c346e172a813cdd7165c1d09
jeronimoherrera918 <jeronimo.herrera.918@estudiante.iesoretania.es>
2021-04-27 08:10:45 +0200

    Class Usuario

---
+++ app/src/main/java/es/iesoretania/entertainmentlounge/Clases/Usuario.java
@@ -0,0 +1,9 @@
+package es.iesoretania.entertainmentlounge.Clases;
+
+public class Usuario {
+    private String email;
+    private String nickname;
+    // private String fechaNacimiento;
+    // private String fotoPerfil; <-- Ver como guardar rutas en Firebase para la foto
+    // private List<String> listaAmigos = new ArrayList<>();
+}
