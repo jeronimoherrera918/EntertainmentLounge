d1b6559bd185bbabc1d881c216e225ae2780f121
jeronimoherrera918 <jeronimo.herrera.918@estudiante.iesoretania.es>
2021-04-28 17:25:20 +0200

    GridView & Test SELECT

--- app/src/main/java/es/iesoretania/entertainmentlounge/Fragmentos/Pruebas/VerSeriesFragment.java
+++ app/src/main/java/es/iesoretania/entertainmentlounge/Fragmentos/Pruebas/VerSeriesFragment.java
@@ -7,27 +7,32 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ListView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import es.iesoretania.entertainmentlounge.Adapters.GridSerieAdapter;
 import es.iesoretania.entertainmentlounge.Adapters.SerieAdapter;
 import es.iesoretania.entertainmentlounge.Clases.SerieData.Serie;
 import es.iesoretania.entertainmentlounge.R;
 
 public class VerSeriesFragment extends Fragment {
     ListView lvListaSeries;
+    GridView gvListaSeries;
 
     FirebaseDatabase database;
     DatabaseReference myRef;
@@ -48,9 +53,29 @@ public class VerSeriesFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         lvListaSeries = view.findViewById(R.id.lvListaSeries);
+        gvListaSeries = view.findViewById(R.id.gvListaSeries);
 
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("series");
+
+        // SELECT * FROM series WHERE genero = "Comedia"
+        /*
+        Query query = (Query) myRef.orderByChild("genero").equalTo("Comedia");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dn : snapshot.getChildren()) {
+                    Serie serie = dn.getValue(Serie.class);
+                    Log.d("TESTING", serie.getNombre());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        */
 
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -64,9 +89,20 @@ public class VerSeriesFragment extends Fragment {
                     listaSeriesKeys.add(dn.getKey());
                 }
 
+                // LIST VIEW
                 SerieAdapter serieAdapter = new SerieAdapter(lvListaSeries.getContext(), R.layout.adapter_series, listaSeries);
                 lvListaSeries.setAdapter(serieAdapter);
                 lvListaSeries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Navigation.findNavController(view).navigate(VerSeriesFragmentDirections.actionNavVerSeriesToSerieFragment(listaSeriesKeys.get(position)));
+                    }
+                });
+
+                // GRID VIEW
+                GridSerieAdapter gridSerieAdapter = new GridSerieAdapter(gvListaSeries.getContext(), R.layout.adapter_grid_series, listaSeries);
+                gvListaSeries.setAdapter(gridSerieAdapter);
+                gvListaSeries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                         Navigation.findNavController(view).navigate(VerSeriesFragmentDirections.actionNavVerSeriesToSerieFragment(listaSeriesKeys.get(position)));
