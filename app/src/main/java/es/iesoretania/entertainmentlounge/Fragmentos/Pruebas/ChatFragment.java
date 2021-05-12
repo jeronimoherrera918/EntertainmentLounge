2f5d66ac7845f5499a407958cbdd5a84d9c89a85
jeronimoherrera918 <jeronimo.herrera.918@estudiante.iesoretania.es>
2021-05-12 23:28:28 +0200

    Enviar mensaje a un usuario específico

--- app/src/main/java/es/iesoretania/entertainmentlounge/Fragmentos/Pruebas/ChatFragment.java
+++ app/src/main/java/es/iesoretania/entertainmentlounge/Fragmentos/Pruebas/ChatFragment.java
@@ -11,23 +11,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.auth.User;
 
+import es.iesoretania.entertainmentlounge.Clases.SerieData.Chat;
 import es.iesoretania.entertainmentlounge.Clases.SerieData.Mensaje;
 import es.iesoretania.entertainmentlounge.Clases.UserData;
+import es.iesoretania.entertainmentlounge.Clases.Usuario;
 import es.iesoretania.entertainmentlounge.R;
 
 public class ChatFragment extends Fragment {
     String keyUser;
     EditText etMensajeUsuario;
+    TextView tvEnviarMensajeA;
     Button btnEnviarMensajeUsuario;
-    // ----- //
-    DocumentReference newRef;
-    String newKey;
-    // ----- //
     FirebaseFirestore db;
 
     @Override
@@ -47,24 +52,52 @@ public class ChatFragment extends Fragment {
         db = FirebaseFirestore.getInstance();
         etMensajeUsuario = view.findViewById(R.id.etMensajeUsuario);
         btnEnviarMensajeUsuario = view.findViewById(R.id.btnEnviarMensajeUsuario);
-
+        tvEnviarMensajeA = view.findViewById(R.id.tvEnviarMensajeA);
         if (getArguments() != null) {
             ChatFragmentArgs chatFragmentArgs = ChatFragmentArgs.fromBundle(getArguments());
             keyUser = chatFragmentArgs.getKeyUser();
         }
+        db.collection("usuarios").document(keyUser).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                Usuario usuario = task.getResult().toObject(Usuario.class);
+                tvEnviarMensajeA.setText("Enviar mensaje a " + usuario.getNickname());
+            }
+        });
+
+        db.collection("usuarios").document(UserData.ID_USER_DB).collection("chats").whereEqualTo("idReceptor", keyUser).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    if (task.getResult().size() == 0) {
+                        Chat chat = new Chat();
+                        chat.setIdReceptor(keyUser);
+                        db.collection("usuarios").document(UserData.ID_USER_DB).collection("chats").document().set(chat);
+                    }
+                }
+            }
+        });
 
         btnEnviarMensajeUsuario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                newRef = db.collection("mensajes").document();
-                newKey = newRef.getId();
-                Mensaje mensaje = new Mensaje();
-                mensaje.setFecha(Timestamp.now());
-                mensaje.setMensaje(etMensajeUsuario.getText().toString());
-                mensaje.setIdEmisor(UserData.ID_USER_DB);
-                mensaje.setIdReceptor(keyUser);
-                mensaje.setIdMensaje(newKey);
-                newRef.set(mensaje);
+                db.collection("usuarios").document(UserData.ID_USER_DB).collection("chats").whereEqualTo("idReceptor", keyUser).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot dn = task.getResult().getDocuments().get(0);
+                            Chat chat = dn.toObject(Chat.class);
+                            Mensaje mensaje = new Mensaje();
+                            mensaje.setIdReceptor(keyUser);
+                            mensaje.setIdEmisor(UserData.ID_USER_DB);
+                            mensaje.setMensaje(etMensajeUsuario.getText().toString());
+                            mensaje.setIdMensaje("s"); // TODO: MODIFICAR ESTO PARA QUE COJA LA ID PARA UN DOCUMENTO
+                            mensaje.setFecha(Timestamp.now());
+                            chat.getListaMensajes().add(mensaje);
+                            db.collection("usuarios").document(UserData.ID_USER_DB).collection("chats").document(dn.getId()).set(chat);
+                        }
+                    }
+                });
             }
         });
     }
