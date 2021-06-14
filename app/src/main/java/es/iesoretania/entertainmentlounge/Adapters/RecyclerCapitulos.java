d56aef21df58199116c6433b8e8c73f2ccde8080
jeronimoherrera918 <jeronimo.herrera.918@estudiante.iesoretania.es>
2021-06-14 14:56:50 +0200

    Update RecyclerCapitulos.java

--- app/src/main/java/es/iesoretania/entertainmentlounge/Adapters/RecyclerCapitulos.java
+++ app/src/main/java/es/iesoretania/entertainmentlounge/Adapters/RecyclerCapitulos.java
@@ -42,7 +42,6 @@ public class RecyclerCapitulos extends RecyclerView.Adapter<RecyclerCapitulos.Vi
     private Context context;
     private String idSerie;
     private int nTemporada;
-    private Animation inAnimation, outAnimation;
     SaveSerie saveSerieGlobal;
     DocumentSnapshot dn;
     FloatingActionButton fabGuardarCambios;
@@ -57,8 +56,6 @@ public class RecyclerCapitulos extends RecyclerView.Adapter<RecyclerCapitulos.Vi
         this.idSerie = idSerie;
         this.nTemporada = nTemporada;
         this.fabGuardarCambios = fabGuardarCambios;
-        inAnimation = AnimationUtils.loadAnimation(context, R.anim.fade_in);
-        outAnimation = AnimationUtils.loadAnimation(context, R.anim.fade_out);
     }
 
     @Override
@@ -84,7 +81,7 @@ public class RecyclerCapitulos extends RecyclerView.Adapter<RecyclerCapitulos.Vi
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView recyclerNombreCapitulo, recyclerPuntuacionMediaCapitulo;
-        ImageButton recyclerbtnMarcarComoVisto, recyclerbtnPuntuarCapitulo;
+        ImageButton recyclerbtnMarcarComoVisto;
 
         public ViewHolder(View v) {
             super(v);
@@ -113,48 +110,42 @@ public class RecyclerCapitulos extends RecyclerView.Adapter<RecyclerCapitulos.Vi
         }
 
         private void activarBotones() {
-            recyclerbtnMarcarComoVisto.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (sw == 0) {
-                        sw = 1;
-                        fabGuardarCambios.setEnabled(true);
-                    }
-                    for (int i = 0; i < saveSerieGlobal.getTemporadas().get(nTemporada).getCapitulos_vistos().size(); i++) {
-                        if (Integer.parseInt(recyclerbtnMarcarComoVisto.getTag().toString()) == i) {
-                            switch (saveSerieGlobal.getTemporadas().get(nTemporada).getCapitulos_vistos().get(Integer.parseInt(recyclerbtnMarcarComoVisto.getTag().toString()))) {
-                                case 1: {
-                                    Animation animation = AnimationUtils.loadAnimation(v.getContext(), R.anim.fade_in);
-                                    recyclerbtnMarcarComoVisto.startAnimation(animation);
-                                    recyclerbtnMarcarComoVisto.setImageResource(R.drawable.ic_check_false);
-                                    saveSerieGlobal.getTemporadas().get(nTemporada).getCapitulos_vistos().set(Integer.parseInt(recyclerbtnMarcarComoVisto.getTag().toString()), 0);
-                                }
-                                break;
-                                case 0: {
-                                    Animation animation = AnimationUtils.loadAnimation(v.getContext(), R.anim.fade_in);
-                                    recyclerbtnMarcarComoVisto.startAnimation(animation);
-                                    recyclerbtnMarcarComoVisto.setImageResource(R.drawable.ic_check_true);
-                                    saveSerieGlobal.getTemporadas().get(nTemporada).getCapitulos_vistos().set(Integer.parseInt(recyclerbtnMarcarComoVisto.getTag().toString()), 1);
-                                }
-                                break;
+            recyclerbtnMarcarComoVisto.setOnClickListener(v -> {
+                if (sw == 0) {
+                    sw = 1;
+                    fabGuardarCambios.setEnabled(true);
+                }
+                for (int i = 0; i < saveSerieGlobal.getTemporadas().get(nTemporada).getCapitulos_vistos().size(); i++) {
+                    if (Integer.parseInt(recyclerbtnMarcarComoVisto.getTag().toString()) == i) {
+                        switch (saveSerieGlobal.getTemporadas().get(nTemporada).getCapitulos_vistos().get(Integer.parseInt(recyclerbtnMarcarComoVisto.getTag().toString()))) {
+                            case 1: {
+                                Animation animation = AnimationUtils.loadAnimation(v.getContext(), R.anim.fade_in);
+                                recyclerbtnMarcarComoVisto.startAnimation(animation);
+                                recyclerbtnMarcarComoVisto.setImageResource(R.drawable.ic_check_false);
+                                saveSerieGlobal.getTemporadas().get(nTemporada).getCapitulos_vistos().set(Integer.parseInt(recyclerbtnMarcarComoVisto.getTag().toString()), 0);
+                            }
+                            break;
+                            case 0: {
+                                Animation animation = AnimationUtils.loadAnimation(v.getContext(), R.anim.fade_in);
+                                recyclerbtnMarcarComoVisto.startAnimation(animation);
+                                recyclerbtnMarcarComoVisto.setImageResource(R.drawable.ic_check_true);
+                                saveSerieGlobal.getTemporadas().get(nTemporada).getCapitulos_vistos().set(Integer.parseInt(recyclerbtnMarcarComoVisto.getTag().toString()), 1);
                             }
                             break;
                         }
+                        break;
                     }
                 }
             });
 
-            fabGuardarCambios.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    /*
-                        TODO: Cuando desmarcamos un episodio como visto desde aquí, hacer las mismas comprobaciones que en CapituloFragment
-                    */
-                    sw = 0;
-                    db.collection("usuarios").document(UserData.ID_USER_DB).collection("series_guardadas").document(dn.getId()).set(saveSerieGlobal);
-                    fabGuardarCambios.setEnabled(false);
-                    Snackbar.make(v, "Cambios guardados correctamente", Snackbar.LENGTH_SHORT).setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE).setBackgroundTint(Color.DKGRAY).show();
-                }
+            fabGuardarCambios.setOnClickListener(v -> {
+                /*
+                    TODO: Cuando desmarcamos un episodio como visto desde aquí, hacer las mismas comprobaciones que en CapituloFragment
+                */
+                sw = 0;
+                db.collection("usuarios").document(UserData.ID_USER_DB).collection("series_guardadas").document(dn.getId()).set(saveSerieGlobal);
+                fabGuardarCambios.setEnabled(false);
+                Snackbar.make(v, "Cambios guardados correctamente", Snackbar.LENGTH_SHORT).setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE).setBackgroundTint(Color.DKGRAY).show();
             });
         }
 
